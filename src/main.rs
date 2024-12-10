e15c706956b594c42aca28af617b37263fccdd08
Mihael Berčič <mihael@regnum.si>
2024-12-10 14:27:12 +0100

    Influx logging sample works.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-use std::env;
-use std::thread::{sleep, spawn};
-use std::time::Duration;
 use dotenv::dotenv;
+use futures::prelude::stream;
+use futures::FutureExt;
+use influxdb2::models::DataPoint;
+use influxdb2::Client;
 use log::{error, warn};
 use logging::logger::init_logging;
 use protocol::connection::run_protocol;
+use std::env;
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+use tokio::macros;
 
 mod logging;
 mod messages;
-mod protocol;
 mod models;
+mod protocol;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let bucket = "hivecore";
+    let host = env::var("INFLUX_HOST").expect("Missing INFLUX_HOST variable.");
+    let organisation = env::var("INFLUX_ORG").expect("Missing INFLUX_ORGA variable.");
+    let token = env::var("INFLUX_TOKEN").expect("Missing INFLUX_TOKEN variable.");
+    let client = Client::new(host, organisation, token);
+    for _ in 0..10 {
+        let points = [DataPoint::builder("cpu")
+            .tag("node", "id-1")
+            .field("speed", 100)
+            .build()?];
+
+        client.write(bucket, stream::iter(points)).await?;
+        println!("Sent one!");
+        sleep(Duration::from_millis(100));
+    }
+    return Ok(());
     let _ = init_logging();
     let _ = dotenv();
 
@@ -19,23 +41,22 @@ fn main() -> anyhow::Result<()> {
         .expect("CONCURRENT_RQEUESTS")
         .parse()
         .unwrap();
-    
 
     let nonce = rand::random::<u64>();
     let reconnection_duration_seconds = 10;
 
     let mut handles = vec![];
     for _ in 0..concurrent {
-        
         let movable_nonce = nonce.clone();
-        let handle = spawn(move || {
-            loop {
-                if let Err(e) = run_protocol(movable_nonce) {
-                    error!("Connection to proxy ended: {}", e);
-                    warn!("Waiting {}s before reconnection.", reconnection_duration_seconds);
-                }
-                sleep(Duration::from_secs(reconnection_duration_seconds));
+        let handle = spawn(move || loop {
+            if let Err(e) = run_protocol(movable_nonce) {
+                error!("Connection to proxy ended: {}", e);
+                warn!(
+                    "Waiting {}s before reconnection.",
+                    reconnection_duration_seconds
+                );
             }
+            sleep(Duration::from_secs(reconnection_duration_seconds));
         });
         handles.push(handle);
     }
@@ -46,3 +67,5 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+async fn submit() {}
