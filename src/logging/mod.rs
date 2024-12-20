1fec8a8e6922b7f22bfcfe64b90e7a67db250925
Mihael Berčič <mihael@regnum.si>
2024-12-20 10:57:46 +0100

    Moved to logging module, added a global function for logging data points and added a shared reference of the influx client.

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -1,1 +1,116 @@
+use std::{
+    env,
+    sync::{Arc, LazyLock, Mutex},
+    thread::{self, sleep},
+    time::Duration,
+};
+
+use futures::stream;
+use influxdb2::{
+    models::{data_point::DataPointBuilder, DataPoint},
+    Client,
+};
+use nvml_wrapper::Nvml;
+use sysinfo::System;
+use tokio::runtime::Handle;
+
+use crate::Error;
+
 pub mod logger;
+
+static INFLUX_CLIENT: LazyLock<Arc<Mutex<Option<InfluxInformation>>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(None)));
+
+struct InfluxInformation {
+    client: Client,
+    node_key: String,
+    tokio_handle: Handle,
+}
+
+pub(crate) fn setup_influx_logging(tokio_handle: Handle) -> Result<(), Error> {
+    let host = env::var("INFLUX_HOST")?;
+    let org = env::var("INFLUX_ORG")?;
+    let token = env::var("INFLUX_TOKEN")?;
+    let client = Client::new(host, org, token);
+    let node_key = env::var("HIVE_KEY")?;
+
+    if let Ok(mut guard) = INFLUX_CLIENT.lock() {
+        *guard = Some(InfluxInformation {
+            client,
+            node_key,
+            tokio_handle,
+        });
+        start_load_logging();
+    }
+    Ok(())
+}
+
+pub(crate) fn log_influx(data: Vec<DataPointBuilder>) {
+    if let Ok(guard) = INFLUX_CLIENT.lock() {
+        if let Some(influx) = &*guard {
+            let clone = influx.client.clone();
+            let data: Vec<DataPoint> = data
+                .into_iter()
+                .filter_map(|x| x.tag("node", &influx.node_key).build().ok())
+                .collect();
+            influx.tokio_handle.spawn(async move {
+                let _ = clone.write("hivecore", stream::iter(data)).await;
+            });
+        }
+    }
+}
+
+fn start_load_logging() {
+    let _ = thread::Builder::new()
+        .name("influx_logging".to_string())
+        .spawn(move || -> Result<(), Error> {
+            let nvml = Nvml::init()?;
+            let mut system = System::new_all();
+            let total_gpus = nvml.device_count()?;
+
+            loop {
+                let mut data_points = vec![];
+
+                for i in 0..total_gpus {
+                    let device = nvml.device_by_index(i)?;
+                    let fan_speed = device.fan_speed(0)?; // Currently 17% on my system
+                    let power_limit = device.enforced_power_limit()?; // 275k milliwatts on my system
+                    let encoder_util = device.encoder_utilization()?; // Currently 0 on my system; Not encoding anything
+                    let memory_info = device.memory_info()?; // Currently 1.63/6.37 GB used on my system
+
+                    data_points.extend([DataPoint::builder("gpu")
+                        .tag("index", i.to_string())
+                        .field("memory_used", memory_info.used as f64)
+                        .field("memory_free", memory_info.free as f64)
+                        .field("memory_total", memory_info.total as f64)
+                        .field("fan_speed", fan_speed as f64)
+                        .field("power_limit", power_limit as f64)
+                        .field("encoder_util", encoder_util.utilization as f64)
+                        .field("sampling_period", encoder_util.sampling_period as f64)
+                        .field(
+                            "energy_consumption",
+                            device.total_energy_consumption()? as f64,
+                        )]);
+                }
+
+                for cpu in system.cpus() {
+                    data_points
+                        .push(DataPoint::builder("cpu").field("usage", cpu.cpu_usage() as f64));
+                }
+
+                data_points.push(
+                    DataPoint::builder("memory")
+                        .field("free", system.free_memory() as f64)
+                        .field("used", system.used_memory() as f64)
+                        .field("total", system.total_memory() as f64)
+                        .field("swap_free", system.free_swap() as f64)
+                        .field("swap_used", system.used_swap() as f64)
+                        .field("swap_total", system.total_swap() as f64),
+                );
+
+                log_influx(data_points);
+                sleep(Duration::from_secs(5));
+                system.refresh_all();
+            }
+        });
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use influxdb2::models::FieldValue;
 use influxdb2::{models::DataPoint, Client};
 use log::{error, warn};
 use logging::logger::init_logging;
+use logging::setup_influx_logging;
 use nvml_wrapper::error::NvmlError;
 use nvml_wrapper::Nvml;
 use protocol::connection::run_protocol;
@@ -24,7 +25,7 @@ mod protocol;
 async fn main() -> anyhow::Result<()> {
     let _ = init_logging();
     let _ = dotenv();
-    start_influx_logging(Handle::current());
+    setup_influx_logging(Handle::current());
 
     let concurrent = env::var("CONCURRENT_RQEUESTS")
         .expect("CONCURRENT_RQEUESTS")
@@ -74,71 +75,4 @@ impl From<NvmlError> for Error {
             message: format!("{:?}", value),
         }
     }
-}
-
-fn start_influx_logging(tokio_handle: Handle) {
-    let _ = thread::Builder::new()
-        .name("influx_logging".to_string())
-        .spawn(move || -> Result<(), Error> {
-            let nvml = Nvml::init()?;
-            let mut system = System::new_all();
-            let host = env::var("INFLUX_HOST")?;
-            let org = env::var("INFLUX_ORG")?;
-            let token = env::var("INFLUX_TOKEN")?;
-            let client = Arc::new(Client::new(host, org, token));
-            let node_key = env::var("HIVE_KEY").expect("Missing HIVE_KEY variable.");
-            let total_gpus = nvml.device_count()?;
-
-            loop {
-                let mut data_points = vec![];
-
-                for i in 0..total_gpus {
-                    let device = nvml.device_by_index(i)?;
-                    let fan_speed = device.fan_speed(0)?; // Currently 17% on my system
-                    let power_limit = device.enforced_power_limit()?; // 275k milliwatts on my system
-                    let encoder_util = device.encoder_utilization()?; // Currently 0 on my system; Not encoding anything
-                    let memory_info = device.memory_info()?; // Currently 1.63/6.37 GB used on my system
-
-                    data_points.extend([DataPoint::builder("gpu")
-                        .tag("index", i.to_string())
-                        .field("memory_used", memory_info.used as f64)
-                        .field("memory_free", memory_info.free as f64)
-                        .field("memory_total", memory_info.total as f64)
-                        .field("fan_speed", fan_speed as f64)
-                        .field("power_limit", power_limit as f64)
-                        .field("encoder_util", encoder_util.utilization as f64)
-                        .field("sampling_period", encoder_util.sampling_period as f64)
-                        .field(
-                            "energy_consumption",
-                            device.total_energy_consumption()? as f64,
-                        )]);
-                }
-
-                for cpu in system.cpus() {
-                    data_points
-                        .push(DataPoint::builder("cpu").field("usage", cpu.cpu_usage() as f64));
-                }
-
-                data_points.push(
-                    DataPoint::builder("memory")
-                        .field("free", system.free_memory() as f64)
-                        .field("used", system.used_memory() as f64)
-                        .field("total", system.total_memory() as f64)
-                        .field("swap_free", system.free_swap() as f64)
-                        .field("swap_used", system.used_swap() as f64)
-                        .field("swap_total", system.total_swap() as f64),
-                );
-
-                let data: Vec<DataPoint> = data_points
-                    .into_iter()
-                    .filter_map(|x| x.tag("node", &node_key).build().ok())
-                    .collect();
-                let clone = client.clone();
-                tokio_handle.spawn(async move {
-                    let _ = clone.write("hivecore", stream::iter(data)).await;
-                });
-                sleep(Duration::from_secs(5));
-                system.refresh_all();
-            }
-        });
 }
