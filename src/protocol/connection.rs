e226ebd04fdfd4044566ee607caba080aecbad22
VakeDomen <vake.domen94@gmail.com>
2025-02-12 13:57:32 +0100

    thread update tags notification

--- src/protocol/connection.rs
+++ src/protocol/connection.rs
@@ -1,60 +1,84 @@
-use std::{env, net::TcpStream, sync::{Arc, Mutex}};
+use std::{env, net::TcpStream, sync::{Arc, Mutex, RwLock}};
 use anyhow::{anyhow, Result};
-use log::info;
+use chrono::{DateTime, Days, Utc};
 use once_cell::sync::Lazy;
 use reqwest::blocking::Client;
+use lazy_static::lazy_static;
 
 use crate::protocol::network_util::{authenticate, create_poller, poll, proxy};
 
+
+
 pub static MODELS: Lazy<Arc<Mutex<String>>> = Lazy::new(|| Arc::new(Mutex::new("/".into())));
 
+lazy_static! {
+    static ref LAST_REFRESH: Arc<RwLock<DateTime<Utc>>> = Arc::new(RwLock::new(Utc::now()));
+}
+
+
 pub fn run_protocol(nonce: u64) -> Result<()> {
-    
-    // Connect to the Proxy Server
     let proxy_server_url = env::var("HIVE_CORE_URL").expect("HIVE_CORE_URL");
     let mut stream = TcpStream::connect(proxy_server_url.clone())?;
-    info!("Establised connection to HiveCore Proxy Server: {}", proxy_server_url);
-
     let client = Client::new();
+    let mut local_refresh_time: DateTime<Utc> = init_local_time();
 
-    if let Err(e) = refresh_poll_models(&client) {
-        return Err(anyhow!(format!("Error refreshing avalible models: {}", e)));
+
+    if let Err(e) = refresh_poll_models(&client, &mut local_refresh_time) {
+        return Err(anyhow!(format!("Error refreshing available models: {}", e)));
     };
 
     if let Err(e) = authenticate(&mut stream, nonce, &client) {
         return Err(anyhow!(format!("Error authenticating: {}", e)));
     };
-
-    info!("Succesfully authenticated to the proxy");
-    let mut has_informed_core_of_tags = false;
+    
 
     loop {
+        let global_refresh_time = get_last_refresh();
+
+        if global_refresh_time > local_refresh_time {
+            if let Err(e) = refresh_poll_models(&client, &mut local_refresh_time) {
+                return Err(anyhow!(format!("Error refreshing models: {}", e)));
+            };
+        }
+
         let models = {
             let models = MODELS.lock().unwrap();
             models.clone()
         };
-        
-        if let Err(e) = poll(&mut stream, models, &has_informed_core_of_tags) {
+
+        if let Err(e) = poll(&mut stream, models, &false) {
             return Err(anyhow!(format!("Error polling HiveCore: {}", e)));
         };
 
-        has_informed_core_of_tags = true;
-    
         let should_refresh = match proxy(&mut stream, &client) {
             Ok(should_refresh) => should_refresh,
             Err(e) => return Err(anyhow!(format!("Failed to proxy request: {}", e))),
         };
 
         if should_refresh {
-            has_informed_core_of_tags = false;
-            refresh_poll_models(&client)?;
+            notify_refresh()
         }
     }
 }
 
-fn refresh_poll_models(client: &Client) -> Result<()> {
+fn notify_refresh() {
+    let mut last_refresh = LAST_REFRESH.write().unwrap();
+    *last_refresh = Utc::now();
+}
+
+fn init_local_time() -> DateTime<Utc> {
+    Utc::now().checked_sub_days(Days::new(1)).unwrap()
+}
+
+fn get_last_refresh() -> DateTime<Utc> {
+    *LAST_REFRESH.read().unwrap()
+}
+
+fn refresh_poll_models(client: &Client, local_last_refresh: &mut DateTime<Utc>) -> Result<()> {
     let poller = create_poller(client)?;
     let mut models = MODELS.lock().unwrap();
     *models = poller.get_models_target();
+    *local_last_refresh = get_last_refresh();
     Ok(())
 }
+
